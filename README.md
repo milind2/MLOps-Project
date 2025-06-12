f00b0f4db07c7c95669a94f0e70f1c744e806f5a
Milind <Milind2Sisodiya@gmail.com>
2025-06-12 00:16:56 -0500

    Project strcture is ready and venv is set

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MLOps-Project
 MLOps-Project for insurance
+
+# virtual env for python name vehicle 
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Milind Sisodiya",
+    author_email="milind02121988@gmail.com",
+    packages=find_packages()
+)
